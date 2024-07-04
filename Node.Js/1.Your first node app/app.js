a45e0d8975cb54b262d15816ac317c27b03b3d66
PANIMALAR S P <57699600+PANIMALARKARTHIK@users.noreply.github.com>
2024-07-04 10:55:30 +0530

    Delete Node.Js directory

--- Node.Js/1.Your first node app/app.js
+++
@@ -1,8 +0,0 @@
-console.log('hi');
-console.log('hello');
-console.log('it\'s me');
-
-//process variable: equivalent of document inside the browser
-//console.log(process);
-console.log(global);
-//setTimeout
